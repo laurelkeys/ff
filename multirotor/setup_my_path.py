9108639cdef75c0b3f8998b0d86d91e2e2727fea
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-10-13 21:42:02 -0300

    Add single drone orbitting

---
+++ multirotor/setup_my_path.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import logging
+
+class SetupPath:
+    @staticmethod
+    def getDirLevels(path):
+        path_norm = os.path.normpath(path)
+        return len(path_norm.split(os.sep))
+
+    @staticmethod
+    def getCurrentPath():
+        cur_filepath = os.path.realpath(__file__) # __file__
+        return os.path.dirname(cur_filepath)
+
+    @staticmethod
+    def getGreatGrandParentDir():
+        cur_path = SetupPath.getCurrentPath()
+        if SetupPath.getDirLevels(cur_path) >= 3:
+            return os.path.dirname(os.path.dirname(cur_path))
+        return ''
+
+    @staticmethod
+    def getGrandParentDir():
+        cur_path = SetupPath.getCurrentPath()
+        if SetupPath.getDirLevels(cur_path) >= 2:
+            return os.path.dirname(os.path.dirname(cur_path))
+        return ''
+
+    @staticmethod
+    def getParentDir():
+        cur_path = SetupPath.getCurrentPath()
+        if SetupPath.getDirLevels(cur_path) >= 1:
+            return os.path.dirname(cur_path)
+        return ''
+
+    @staticmethod
+    def addAirSimModulePath():
+        parent = SetupPath.getParentDir()
+        if parent !=  '':
+            airsim_path = os.path.join(parent, 'airsim')
+            client_path = os.path.join(airsim_path, 'client.py')
+            print(client_path)
+            if os.path.exists(client_path):
+                sys.path.insert(0, parent)
+        else:
+            logging.warning("airsim module not found in parent folder. Using installed package (pip install airsim).")
+
+SetupPath.addAirSimModulePath()
+
+if __name__ == "__main__":
+    cur_path = SetupPath.getCurrentPath()
+    print("getCurrentPath", cur_path)
+    print("getDirLevels", SetupPath.getDirLevels(cur_path))
+    print("getParentDir", SetupPath.getParentDir())
+    print("getGrandParentDir", SetupPath.getGrandParentDir())
+    print("getGreatGrandParentDir", SetupPath.getGreatGrandParentDir())
