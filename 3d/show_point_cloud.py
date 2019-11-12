db0a326169941999a62485cbbdb1407f89d6da45
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-11-12 13:52:12 -0300

    Add Open3d screen captures

--- 3d/show_point_cloud.py
+++ 3d/show_point_cloud.py
@@ -3,7 +3,7 @@ import numpy as np
 import open3d as o3d
 
 def main(fname, bbox_scale=1):
-    pcd = o3d.io.read_point_cloud()
+    pcd = o3d.io.read_point_cloud(fname)
     aabb = pcd.get_axis_aligned_bounding_box()
 
     # coordinate axis
