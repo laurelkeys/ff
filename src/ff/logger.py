8509b44f392827e8ea77ba9bf1b69e817e0f4c26
Tiago <tiagoloureirochaves@gmail.com>
2020-05-25 22:33:16 -0300

    Start ff v2 with basic files in src/

---
+++ src/ff/logger.py
@@ -0,0 +1,28 @@
+from .types import to_xyz_str, to_xyzw_str, angles_to_str
+
+
+###############################################################################
+###############################################################################
+
+
+def print_airsim_path(airsim_path):
+    path_str = f"'airsim' path: {airsim_path[0]}"
+    print("-" * len(path_str), path_str, "-" * len(path_str), sep="\n")
+
+
+def print_pose(vehicle_pose, to_eularian_angles=None):
+    print_str = [
+        "[ff] VehiclePose \n",
+        f"     .position    = {to_xyz_str(vehicle_pose.position)}\n",
+        f"     .orientation = {to_xyzw_str(vehicle_pose.orientation)}\n",
+        f"                    {angles_to_str(to_eularian_angles(vehicle_pose.orientation))}\n",
+    ]
+
+    if to_eularian_angles is None:
+        print_str = print_str[:-1]
+
+    print(*print_str)
+
+
+###############################################################################
+###############################################################################
---
+++ src/settings.py
@@ -0,0 +1,10 @@
+
+env_uproject_path = "D:\\dev\\UE4\\Custom Environments\\Cidadela 4.24\\Cidadela.uproject"
+ue4editor_exe_path = "D:\\Program Files\\Epic Games\\UE_4.24\\Engine\\Binaries\\Win64\\UE4Editor.exe"
+
+move_by_velocity = True
+move_by_position = not move_by_velocity
+
+game_resX = 1280
+game_resY = 720
+windowed = True
