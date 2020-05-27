edf9872d5d1803dd3dbe76909d7fb686b5e0260e
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-05-27 11:54:34 -0300

    Replace f-strings with os.path.join

--- src/ff/helper.py
+++ src/ff/helper.py
@@ -1,18 +1,19 @@
 import os
 import sys
 import json
-import subprocess
+
+from glob import glob
+from os.path import join
 
 from .sim import SimMode
 from .defaults import Default
-
 
 ###############################################################################
 ###############################################################################
 
 
 def add_airsim_to_path(airsim_path):
-    assert os.path.exists(os.path.join(airsim_path, "client.py")), airsim_path
+    assert os.path.exists(join(airsim_path, "client.py")), airsim_path
     sys.path.insert(0, os.path.dirname(airsim_path))
 
 
@@ -20,13 +21,13 @@ def add_airsim_to_path(airsim_path):
 ###############################################################################
 
 
-def curr_sim_mode(settings_path=Default.SETTINGS_PATH):
+def curr_sim_mode(settings_path=Default.AIRSIM_SETTINGS_PATH):
     with open(settings_path, "r") as settings_file:
         settings = json.load(settings_file)
     return settings["SimMode"]
 
 
-def change_sim_mode(new_sim_mode, settings_path=Default.SETTINGS_PATH):
+def change_sim_mode(new_sim_mode, settings_path=Default.AIRSIM_SETTINGS_PATH):
     """ Returns a tuple `(changed_sim_mode: bool, curr_sim_mode: str)`. """
 
     assert new_sim_mode in SimMode._list_all, f"\ninvalid SimMode '{new_sim_mode}'\n"
@@ -48,15 +49,15 @@ def change_sim_mode(new_sim_mode, settings_path=Default.SETTINGS_PATH):
 ###############################################################################
 
 
-def possible_env_paths(env_root, exts=["*.exe", "*.sln", "*.uproject"]):
+def possible_env_paths(env_root, exts=["exe", "sln", "uproject"]):
     """ Searches for valid environment files with the following patterns:
         - `env_root/*.ext`
         - `env_root/*/*.ext`
     """
     env_paths = []
     for ext in exts:
-        env_paths.extend(glob.glob(f"{env_root}/*.{ext}"))
-        env_paths.extend(glob.glob(f"{env_root}/*/*.{ext}"))
+        env_paths.extend(glob(join(env_root, f"*.{ext}")))
+        env_paths.extend(glob(join(env_root, "*", f"*.{ext}")))
     return env_paths
 
 
