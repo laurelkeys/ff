fd9c82852219d3026d1bc73ce14b81302219cf41
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-06-29 11:25:33 -0300

    Import vendor files

--- src/wrappers/__ff_to_path__.py
+++ src/wrappers/__ff_to_path__.py
@@ -1,10 +1,16 @@
 import os
 import sys
 
-__abs_dir_path = os.path.abspath(os.path.dirname(__file__))
-__ff_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "ff")
-sys.path.insert(0, __ff_path)
+__ff_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "..", "ff"
+)
+
+sys.path.insert(0,  os.path.dirname(__ff_path))
+
+import ff
+
+del sys.path[0], sys, os
 
 if __name__ == "__main__":
-    print(f"abs_dir_path = '{__abs_dir_path}'")
-    print(f"ff_path = '{__ff_path}'")
+    print(f"ff path = '{__ff_path}'")
