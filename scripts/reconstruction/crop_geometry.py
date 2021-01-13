c6eda61fa43de8dc643cbf0b67b10d3cd458999c
Tiago Chaves <tiagoloureirochaves@gmail.com>
2021-01-13 09:31:44 -0300

    Get crop file from v1

---
+++ scripts/reconstruction/crop_geometry.py
@@ -0,0 +1,28 @@
+import argparse
+
+import numpy as np
+import open3d as o3d
+
+
+def crop_geometry(ply_path: str) -> None:
+    pcd = o3d.io.read_point_cloud(ply_path)
+    # np_pcd = np.asarray(pcd.points)
+    center = pcd.get_center()
+    aabb = pcd.get_axis_aligned_bounding_box()
+    print(f"Center: {center}\n{aabb}")
+
+    print("Manual geometry cropping")
+    print("1) Press 'X', 'Y' or 'Z' to align the geometry with an orthogonal axis")
+    print("2) Press 'K' to lock the screen and switch to selection mode")
+    print("3) Drag for rectangle selection, or use Ctrl+LMB for polygon selection")
+    print("4) Press 'C' to get a selected geometry and to save it")
+    print("5) Press 'F' to switch to freeview mode")
+
+    o3d.visualization.draw_geometries_with_editing([pcd])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Interactively crop a PLY file")
+    parser.add_argument("ply_path", type=str, help="Path to the PLY file")
+    args = parser.parse_args()
+    crop_geometry(args.ply_path)
