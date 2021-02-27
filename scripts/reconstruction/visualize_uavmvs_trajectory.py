7358c2a299cf1db913539c12ff6a4eba8d31438a
Tiago Chaves <tiagoloureirochaves@gmail.com>
2021-02-27 11:52:16 -0300

    WIP parse .utj trajectory files

--- scripts/reconstruction/visualize_uavmvs_trajectory.py
+++ scripts/reconstruction/visualize_uavmvs_trajectory.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 
+from enum import Enum
 from typing import Optional, NamedTuple
 
 import numpy as np
@@ -11,35 +12,137 @@ import open3d as o3d
 ###############################################################################
 
 
-def _parse_float_list(line):
-    return [float(_) for _ in line.split()]
+class TrajectoryCameraKind(Enum):
+    Traj = 0
+    Csv = 1
+    Utj = 2
+
+    # NOTE while the units and coordinate system for .traj
+    # and .csv files are the same, in .utj files the x sign
+    # is inverted, and x, y, z are in centimeters.
 
 
-def _eat_position(f):
-    position = np.array(_parse_float_list(f.readline()))
-    assert position.shape == (3,)
-    return position
+TRAJ = TrajectoryCameraKind.Traj
+CSV = TrajectoryCameraKind.Csv
+UTJ = TrajectoryCameraKind.Utj
 
 
-def _eat_rotation(f):
-    rotation = np.array(
-        [
-            _parse_float_list(f.readline()),
-            _parse_float_list(f.readline()),
-            _parse_float_list(f.readline()),
-        ]
-    )
-    assert rotation.shape == (3, 3)
-    return rotation
+CSV_HEADER = "x,y,z,qw,qx,qy,qz,key"
+INTO_UTJ = np.array([-100, 100, 100])  # m -> cm and invert x
+FROM_UTJ = np.array([-0.01, 0.01, 0.01])  # cm -> m and invert x
 
 
 class TrajectoryCamera(NamedTuple):
-    """ Represents a camera pose entry in a .traj (or .csv) file. """
+    """ Represents a camera pose entry in a .traj, .csv or .utj file. """
 
     position: np.ndarray
-    rotation: np.ndarray  # NOTE (3, 3) rotation matrix or (4,) xyzw quaternion
-    interpolated: bool = False  # NOTE spline interpolated position if True
-    focal_length: Optional[float] = None
+    rotation: np.ndarray
+    kind: TrajectoryCameraKind
+    focal_length: Optional[float] = None  # .traj
+    spline_interpolated: bool = False  # .csv
+    id: Optional[int] = None  # .utj
+
+    # NOTE `rotation` is:
+    # - (3, 3) rotation matrix, if kind == TRAJ
+    # - (4,) XYZW quaternion, if kind == CSV
+    # - (3,) pitch,roll,yaw, if kind == UTJ
+
+    def _rotation_into(self, other_kind):
+        if self.kind == other_kind:
+            return self.rotation
+        # def matrix_to_quat(): raise NotImplementedError
+        # def matrix_to_euler(): raise NotImplementedError
+        # def quat_to_matrix(): raise NotImplementedError
+        # def quat_to_euler(): raise NotImplementedError
+        # def euler_to_matrix(): raise NotImplementedError
+        # def euler_to_quat(): raise NotImplementedError
+        # return {
+        #     (TRAJ, CSV): matrix_to_quat,
+        #     (TRAJ, UTJ): matrix_to_euler,
+        #     (CSV, TRAJ): quat_to_matrix,
+        #     (CSV, UTJ): quat_to_euler,
+        #     (UTJ, TRAJ): euler_to_matrix,
+        #     (UTJ, CSV): euler_to_quat,
+        # }[(self.kind, other_kind)]()
+        raise NotImplementedError
+
+    def into(self, other_kind):
+        if self.kind in [TRAJ, CSV] and other_kind in [TRAJ, CSV]:
+            return self
+        elif self.kind != UTJ:
+            return TrajectoryCamera(
+                position=self.position * FROM_UTJ,
+                rotation=self._rotation_into(other_kind),
+                kind=other_kind
+            )
+        elif other_kind != UTJ:
+            return TrajectoryCamera(
+                position=self.position * INTO_UTJ,
+                rotation=self._rotation_into(other_kind),
+                kind=other_kind
+            )
+        return self
+
+
+###############################################################################
+###############################################################################
+
+
+def parse_uavmvs_traj(filepath):
+    trajectory = []
+    with open(filepath, "r") as f:
+        n_of_cameras = int(f.readline())
+        for _ in range(n_of_cameras):
+            position = np.array([float(_) for _ in f.readline()])
+            rotation = np.array(
+                [
+                    [float(_) for _ in f.readline()],
+                    [float(_) for _ in f.readline()],
+                    [float(_) for _ in f.readline()],
+                ]
+            )
+            focal_length = float(f.readline())
+            assert position.shape == (3,)
+            assert rotation.shape == (3, 3)
+            trajectory.append(
+                TrajectoryCamera(
+                    position, rotation, kind=TRAJ, focal_length=focal_length
+                )
+            )
+    return trajectory
+
+
+def parse_uavmvs_csv(filepath):
+    trajectory = []
+    with open(filepath, "r") as f:
+        assert f.readline().rstrip().lower() == CSV_HEADER
+        for line in f.readlines():
+            x, y, z, qw, qx, qy, qz, key = line.split(",")
+            position = np.array([float(_) for _ in [x, y, z]])
+            rotation = np.array([float(_) for _ in [qx, qy, qz, qw]])
+            trajectory.append(
+                TrajectoryCamera(
+                    position, rotation, kind=CSV, spline_interpolated=(int(key) == 0),
+                )
+            )
+    return trajectory
+
+
+def parse_uavmvs_utj(filepath):
+    trajectory = []
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            id, x, y, z, pitch, roll, yaw = line.split(",")
+            # NOTE angles are in euler degrees (standard for UE4),
+            # x, y and z are in centimeters and x sign is inverted.
+            position = np.array([float(_) for _ in [x, y, z]])
+            rotation = np.array([float(_) for _ in [pitch, roll, yaw]])
+            trajectory.append(
+                TrajectoryCamera(
+                    position, rotation, kind=UTJ, id=int(id),
+                )
+            )
+    return trajectory
 
 
 ###############################################################################
@@ -48,7 +151,6 @@ class TrajectoryCamera(NamedTuple):
 
 RGB = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 CMY = [[0, 1, 1], [1, 0, 1], [1, 1, 0]]
-
 BLACK = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 WHITE = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 
@@ -89,31 +191,16 @@ def main(args: argparse.Namespace) -> None:
     if args.verbose:
         o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
 
-    _, ext = os.path.splitext(args.trajectory)
-    assert os.path.isfile(args.trajectory) and ext in [".traj", ".csv"]
+    parse_uavmvs = {
+        ".traj": parse_uavmvs_traj,
+        ".csv": parse_uavmvs_csv,
+        ".utj": parse_uavmvs_utj,
+    }
 
-    with open(args.trajectory, "r") as f:
-        trajectory = []
-        if ext == ".traj":
-            n_of_cameras = int(f.readline())
-            for _ in range(n_of_cameras):
-                position = _eat_position(f)
-                rotation = _eat_rotation(f)
-                trajectory.append(
-                    TrajectoryCamera(position, rotation, focal_length=float(f.readline()))
-                )
-            assert len(trajectory) == n_of_cameras
-        else:
-            f.readline()  # skip header
-            for line in f.readlines():
-                x, y, z, qw, qx, qy, qz, key = line.split(",")
-                trajectory.append(
-                    TrajectoryCamera(
-                        position=np.array([x, y, z]),
-                        rotation=np.array([qx, qy, qz, qw]),
-                        interpolated=(int(key) == 0),
-                    )
-                )
+    _, ext = os.path.splitext(args.trajectory)
+    assert os.path.isfile(args.trajectory) and ext in parse_uavmvs.keys()
+
+    trajectory = parse_uavmvs[ext](args.trajectory)
 
     camera_positions = [camera.position for camera in trajectory]
 
@@ -158,7 +245,7 @@ if __name__ == "__main__":
         description="Visualize a trajectory generated by uavmvs with Open3D."
     )
 
-    parser.add_argument("trajectory", type=str, help="Path to the input .TRAJ (or .CSV) file")
+    parser.add_argument("trajectory", type=str, help="Path to the input .TRAJ, .CSV or .UTJ file")
     parser.add_argument("--export", type=str, help="Path to export the trajectory as .PLY to")
 
     parser.add_argument("--mesh", type=str, help="Path to a .PLY mesh file")
