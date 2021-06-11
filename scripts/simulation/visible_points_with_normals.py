fd9222eae169b802dacfd33449b773f79e348623
Tiago Chaves <tiagoloureirochaves@gmail.com>
2021-06-11 20:22:34 -0300

    WIP improve hidden point removal (HPR)

--- scripts/simulation/visible_points_with_normals.py
+++ scripts/simulation/visible_points_with_normals.py
@@ -13,20 +13,18 @@ class CameraVisible(NamedTuple):
     closest_point: np.ndarray
     closest_point_distance: float
     spherical_projection_radius: float
+    points_indices_in_original_pcd: np.ndarray
 
 
 def visible_points_with_normals(
     camera_position: np.ndarray,
-    # camera_orientation_xyzw: np.ndarray,
     pcd_points: np.ndarray,
     transform_pcd_points: Optional[np.ndarray],
-    spherical_projection_radius: Optional[float] = None,
+    spherical_projection_radius_factor: float = 1.0,
 ) -> CameraVisible:
-    assert camera_position.shape == (3,)
-    # assert camera_orientation_xyzw.shape == (4,)
-
     n, _ = pcd_points.shape
     assert pcd_points.shape[1] == 3
+    assert camera_position.shape == (3,)
 
     if transform_pcd_points is None:
         points = pcd_points
@@ -43,10 +41,9 @@ def visible_points_with_normals(
     closest_point = points[np.argmin(points_squared_distance_to_camera)]
     closest_point_distance = np.sqrt(np.min(points_squared_distance_to_camera))
 
-    if spherical_projection_radius is None:
-        # FIXME this is a bad value guess for noisy point clouds
-        spherical_projection_radius = closest_point_distance
-        spherical_projection_radius *= 30  # FIXME
+    # FIXME there are better ways than guessing this value for noisy point clouds
+    spherical_projection_radius = closest_point_distance
+    spherical_projection_radius *= spherical_projection_radius_factor
 
     import open3d as o3d
     import ie.open3dy as o3dy
@@ -75,7 +72,7 @@ def visible_points_with_normals(
     del o3d
 
     return CameraVisible(
-        points, normals, closest_point, closest_point_distance, spherical_projection_radius
+        points, normals, closest_point, closest_point_distance, spherical_projection_radius, pt_map
     )
 
 
@@ -87,8 +84,9 @@ if __name__ == "__main__":
     import airsim
     import open3d as o3d
     import ie.airsimy as airsimy
+    import ie.open3dy as o3dy
 
-    MAX_POINTS = 50_000
+    MAX_POINTS = 10_000
 
     pcd = o3d.io.read_point_cloud(sys.argv[1], print_progress=True)
     print(f"Point cloud has {len(pcd.points)} points")
@@ -98,7 +96,7 @@ if __name__ == "__main__":
         pcd = pcd.uniform_down_sample(every_k_points=k)
         print(f"Downsampled to {len(pcd.points)} points")
 
-    pcd_points = np.asarray(pcd.points)
+    pcd_points = np.asarray(pcd.points)  # XXX flipping Y and Z coordinates
     pcd_points[:, 1] *= -1
     pcd_points[:, 2] *= -1
 
@@ -108,34 +106,44 @@ if __name__ == "__main__":
         align_pcd_to_airsim = None
 
     client = airsimy.connect(ff.SimMode.ComputerVision)
-    pose = client.simGetVehiclePose()
-
     # client = airsimy.connect(ff.SimMode.Multirotor)
-    # pose = client.simGetCameraInfo(ff.CameraName.front_center).pose
-
     client.simFlushPersistentMarkers()
 
-    print(f"{pose = }")
-    print(f"{pcd = }")
-    print(f"{align_pcd_to_airsim = }")
+    camera = client.simGetCameraInfo(ff.CameraName.front_center)
+    # camera = client.simGetCameraInfo(ff.CameraName.bottom_center)
+
+    # NOTE the orientation is not considered for hidden point removal, so we could
+    # use either the vehicle pose or of one the cameras's. However, if we consider
+    # the (horizontal) FOV of the camera we can remove points that are out of view.
+    camera_position = camera.pose.position.to_numpy_array()
+
+    print(f"FOV = {camera.fov} degrees")
+    print(f"Projection matrix = {camera.proj_mat}")
+    print(f"Orientation (XYZW) = {camera.pose.orientation.to_numpy_array()}")
+
+    # TODO remove pcd_points that are outside of the camera's view frustum by using this values:
+    # front_axis, right_axis, up_axis = airsimy.AirSimNedTransform.local_axes_frame(camera.pose)
 
     client.simPlotPoints([airsim.Vector3r(*p) for p in pcd_points], [1, 0, 0, 1], 3, -1, True)
 
     visible = visible_points_with_normals(
-        pose.position.to_numpy_array(),
+        camera_position,
         pcd_points,
         align_pcd_to_airsim,
-        spherical_projection_radius=None,
+        spherical_projection_radius_factor=100.0,
     )
 
-    print(f"{visible = }")
     visible_points = [airsim.Vector3r(*p) for p in visible.points]
     visible_normals = [airsim.Vector3r(*(p + n)) for p, n in zip(visible.points, visible.normals)]
+
     client.simPlotPoints(visible_points, [0, 0, 1, 1], 6, -1, True)
     client.simPlotArrows(visible_points, visible_normals, [0, 1, 0, 1], 2, 100, -1, True)
 
-    # TODO plot everything inside AirSim
+    # pcd.orient_normals_towards_camera_location(pose.position.to_numpy_array())
+    # visible_pcd, not_visible_pcd = o3dy.split_points_by_index(pcd, visible.points_indices_in_original_pcd)
+    # visible_pcd.paint_uniform_color([0, 0, 1])
+    # not_visible_pcd.paint_uniform_color([1, 0, 0])
+    # o3d.visualization.draw_geometries([visible_pcd])
+    # o3d.visualization.draw_geometries([visible_pcd, not_visible_pcd])
 
-    # client.reset()
-
-    del sys, ff, np, airsim, o3d, airsimy
+    del sys, ff, np, airsim, o3d, airsimy, o3dy
